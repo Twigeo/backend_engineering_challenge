dc3fce893069990686a6e248cbd2d7f79c3fe4b1
Ammar sahyoun <ammarzgr@hotmail.com>
2021-10-15 23:52:02 +0200

    add EDA function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import gspread, os
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def service_acct(cred):
     if os.path.isfile(cred) and os.access(cred, os.R_OK):
@@ -9,18 +11,38 @@ def service_acct(cred):
             service_acc = None
 
     else:
-        print('You need credentials file to run this app!')
         service_acc = None
     return service_acc
 
+def get_key(url):
+    id = url.split('/')[-2]
+    return id
+
+
+def eda(df):
+    df['Date'] = pd.to_datetime(df.Date, infer_datetime_format=True) #covnert date object to date_format
+    df.rename(columns={"Daily Active Users": "dau", "Subscription started": "sub_started"}, inplace=True)
+    df.set_index('Date', inplace=True)
+
+    #list of the unique dates
+    unique_dates = list(df.index.unique())
+
+    #list of the unique networks
+    networks = list(df.Network.unique())
+    return unique_dates, networks
+
 
 def main():
-    credentials = "twigeo-credentials.json"
-    service_acc = service_acct(credentials)
-    workbook = service_acc.open_by_key("1nRteMJI2lE05AFs4fSTxEvCXnbRLo1dmNjC1xC0IuZU").sheet1
-    data = workbook.get_all_records()
 
-    # print(data)
+    service_acc = service_acct("twigeo-credentials.json")
+    sheet_key = get_key("https://docs.google.com/spreadsheets/d/1nRteMJI2lE05AFs4fSTxEvCXnbRLo1dmNjC1xC0IuZU/edit#gid=910743533")
+    workbook = service_acc.open_by_key(sheet_key).sheet1
+    data = workbook.get_all_records()
+    dataframe = pd.DataFrame.from_dict(data)
+
+
+
+
 
 
 if __name__ == "__main__":
