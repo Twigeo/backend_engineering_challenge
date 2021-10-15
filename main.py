93e42b792a6fe6abfb5f016339a170010d54d1ae
Ammar sahyoun <ammarzgr@hotmail.com>
2021-10-16 00:40:53 +0200

    add plot function

--- main.py
+++ main.py
@@ -32,13 +32,36 @@ def eda(df):
     return unique_dates, networks
 
 
+def network_dau(dates, networks, df):
+    network_daily_user = []
+    for date in dates:
+        d = df[df.index == date] #data frame for each day
+        network_daily_user.append({i: d[d.Network == i].dau.sum() for i in networks}) #dau for each network
+    network_daily_user = pd.DataFrame.from_dict(network_daily_user)
+    network_daily_user.index = dates
+    return network_daily_user
+
+def plot_it(df):
+    network = df.eq(df.max(1), axis=0).dot(df.columns)[1] #the network that has the max value
+    sns.set_style("darkgrid")
+    plt.figure(figsize=(12, 5))
+    sns.lineplot(data=df).set_title(f"{network} usually has the most active users on a daily basis")
+
+
 def main():
 
     service_acc = service_acct("twigeo-credentials.json")
     sheet_key = get_key("https://docs.google.com/spreadsheets/d/1nRteMJI2lE05AFs4fSTxEvCXnbRLo1dmNjC1xC0IuZU/edit#gid=910743533")
     workbook = service_acc.open_by_key(sheet_key).sheet1
     data = workbook.get_all_records()
+    print('fetch data from the sheet... Done!')
     dataframe = pd.DataFrame.from_dict(data)
+
+    unique_dates, networks = eda(dataframe)
+    #create new dataframe for each unique day
+    net_dau = network_dau(unique_dates, networks, dataframe)
+    plot_it(net_dau)
+
 
 
 
